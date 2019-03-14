498f76333ce1aa631c0a69db6f807e0591b871c6
Christopher Di Bella <cjdb.ns@gmail.com>
2019-03-14 10:26:35 +0000

    added a basic install script

--- conanfile.py
+++ conanfile.py
@@ -18,12 +18,15 @@ import os
 
 
 class Project_name(ConanFile):
-    name = "project_name"
+    name = "basic_project"
+    description = "A project starter for C++ projects."
+    license = "Apache License, Version 2.0"
     version = "1.0"
     settings = "os", "compiler", "arch", "build_type"
     generators = "cmake", "cmake_paths", "virtualrunenv"
     requires = "range-v3/v1.0-beta@cjdb/beta", "doctest/2.2.0@bincrafters/stable", "boost/1.69.0@conan/stable"
-    exports_sources = "CMakeLists.txt", ".clang*", "include/*", "test/*", "src/*", "cmake/*"
+    exports_sources = ".clang*", "cmake/*", "CMakeLists.txt", "include/*", "source/*", "test/*", "LICENSE.md"
+    build_policy = "missing"
     no_copy_source = True
 
     def build(self):
@@ -36,6 +39,14 @@ class Project_name(ConanFile):
         with tools.environment_append(env_build.vars):
             if self.should_test:
                 self.run("ctest -j %s --output-on-failure" % tools.cpu_count())
+
+    def package(self):
+        cmake = CMake(self)
+
+        cmake.definitions["BUILD_TESTING"] = "OFF"
+        cmake.configure()
+        cmake.install()
+        self.copy("LICENSE.md", dst="licenses", ignore_case=True, keep_path=False)
 
     def package_info(self):
         self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
