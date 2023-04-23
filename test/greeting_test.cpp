7e4babd78e09270b821b5ab368b0ea1747aac5cd
Christopher Di Bella <cjdb.ns@gmail.com>
2023-04-22 18:40:11 -0700

    updates Catch2 usage
    Catch2 v3 has changed how it is used.

--- test/greeting_test.cpp
+++ test/greeting_test.cpp
@@ -1,5 +1,5 @@
 #include <project_template/greeting.hpp>
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 #include <string_view>
 
 TEST_CASE("greeting returns expected values")
